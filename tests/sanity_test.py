e7691aa7067bda4836d4a8d202572b4cd5b0b07f
Hemant2388 <hemant7602@gmail.com>
2025-06-27 07:41:07 +0000

    fixes

--- tests/sanity_test.py
+++ tests/sanity_test.py
@@ -1,11 +1,16 @@
+import joblib
+import pandas as pd
+
 def test_model_prediction():
     model = joblib.load("artifacts/model.joblib")
-
+    
+    # A sample test input (from setosa class)
     test_sample = pd.DataFrame([[5.1, 3.5, 1.4, 0.2]], columns=[
         'sepal_length', 'sepal_width', 'petal_length', 'petal_width'
     ])
-
+    
     prediction = model.predict(test_sample)
+    
     print("Predicted species:", prediction[0])  # 👈 This will show output
-
+    
     assert prediction[0] in ['setosa', 'versicolor', 'virginica']
